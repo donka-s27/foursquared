834d6ff66f08ebbe5e371658bad6bdb1879deca9
Joe LaPenna <joe@joelapenna.com>
2009-12-03 09:33:44 -0800

    force simple if statements on one line.

---
+++ main/src/com/joelapenna/foursquare/types/CheckinResult.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2009 Joe LaPenna
+ */
+
+package com.joelapenna.foursquare.types;
+
+/**
+ * Auto-generated: 2009-11-12 21:45:35.708780
+ *
+ * @author Joe LaPenna (joe@joelapenna.com)
+ */
+public class CheckinResult implements FoursquareType {
+
+    private Group mBadges;
+    private String mCreated;
+    private String mId;
+    private Mayor mMayor;
+    private String mMessage;
+    private Scoring mScoring;
+    private Venue mVenue;
+
+    public CheckinResult() {
+    }
+
+    public Group getBadges() {
+        return mBadges;
+    }
+
+    public void setBadges(Group badges) {
+        mBadges = badges;
+    }
+
+    public String getCreated() {
+        return mCreated;
+    }
+
+    public void setCreated(String created) {
+        mCreated = created;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public Mayor getMayor() {
+        return mMayor;
+    }
+
+    public void setMayor(Mayor mayor) {
+        mMayor = mayor;
+    }
+
+    public String getMessage() {
+        return mMessage;
+    }
+
+    public void setMessage(String message) {
+        mMessage = message;
+    }
+
+    public Scoring getScoring() {
+        return mScoring;
+    }
+
+    public void setScoring(Scoring scoring) {
+        mScoring = scoring;
+    }
+
+    public Venue getVenue() {
+        return mVenue;
+    }
+
+    public void setVenue(Venue venue) {
+        mVenue = venue;
+    }
+
+}
---
+++ main/src/com/joelapenna/foursquared/StatsActivity.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2009 Joe LaPenna
+ */
+
+package com.joelapenna.foursquared;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * @author Joe LaPenna (joe@joelapenna.com)
+ */
+public class StatsActivity extends Activity {
+    public static final String TAG = "StatsActivity";
+    public static final boolean DEBUG = FoursquaredSettings.DEBUG;
+
+    private BroadcastReceiver mLoggedOutReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (DEBUG) Log.d(TAG, "onReceive: " + intent);
+            finish();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_PROGRESS);
+        setContentView(R.layout.stats_activity);
+        registerReceiver(mLoggedOutReceiver, new IntentFilter(Foursquared.INTENT_ACTION_LOGGED_OUT));
+
+        setTitle("Foursquare Scoreboard");
+
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebChromeClient(new MyWebChromeClient());
+
+        String userId = ((Foursquared)getApplication()).getUserId();
+        String cityId = ((Foursquared)getApplication()).getUserCity().getId();
+
+        String url = "http://foursquare.com./iphone/me?view=all&scope=friends&uid=" + userId
+                + "&cityid=" + cityId;
+        Log.d(TAG, url);
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mLoggedOutReceiver);
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            setProgress(newProgress * 100);
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            setProgressBarVisibility(false);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            setProgressBarVisibility(true);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
