c97b9446c53e8dcb139f1091c457603d03a66d84
markww <markww@gmail.com>
2010-09-02 11:24:13 -0700

    Working on new tip details activity. Stopping here to implement switch over to json for new endpoints they require.

--- main/src/com/joelapenna/foursquared/TipActivity.java
+++ main/src/com/joelapenna/foursquared/TipActivity.java
@@ -6,11 +6,7 @@ package com.joelapenna.foursquared;
 
 import com.joelapenna.foursquare.Foursquare;
 import com.joelapenna.foursquare.types.Tip;
-import com.joelapenna.foursquare.types.User;
 import com.joelapenna.foursquare.types.Venue;
-import com.joelapenna.foursquared.util.RemoteResourceManager;
-import com.joelapenna.foursquared.util.StringFormatters;
-import com.joelapenna.foursquared.widget.TipActivityAdapter;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
@@ -18,26 +14,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
-
-import java.io.IOException;
-import java.util.Observable;
-import java.util.Observer;
 
 /**
  * Shows actions available for a tip:
@@ -61,19 +46,11 @@ public class TipActivity extends Activity {
 
     public static final String EXTRA_TIP_PARCEL = Foursquared.PACKAGE_NAME
         + ".TipActivity.EXTRA_TIP_PARCEL";
-
-    public static final String EXTRA_VENUE_NAME = Foursquared.PACKAGE_NAME
-        + ".TipActivity.EXTRA_VENUE_NAME";
     
     public static final int RESULT_TIP_MARKED_TODO = -2;
     public static final int RESULT_TIP_MARKED_DONE = -3;
     
-    private TipActivityAdapter mListAdapter;
     private StateHolder mStateHolder;
-    private ListView mListView;
-    private Handler mHandler;
-    private RemoteResourceManager mRrm;
-    private RemoteResourceManagerObserver mResourcesObserver;
     private ProgressDialog mDlgProgress;
     
 
@@ -91,9 +68,6 @@ public class TipActivity extends Activity {
         setContentView(R.layout.tip_activity);
         registerReceiver(mLoggedOutReceiver, new IntentFilter(Foursquared.INTENT_ACTION_LOGGED_OUT));
         
-        mRrm = ((Foursquared) getApplication()).getRemoteResourceManager();
-        mHandler = new Handler();
-        
         Object retained = getLastNonConfigurationInstance();
         if (retained != null && retained instanceof StateHolder) {
             mStateHolder = (StateHolder) retained;
@@ -105,15 +79,6 @@ public class TipActivity extends Activity {
                 
                 Tip tip = getIntent().getExtras().getParcelable(EXTRA_TIP_PARCEL);
                 mStateHolder.setTip(tip);
-                
-                if (getIntent().getExtras().containsKey(EXTRA_VENUE_NAME)) {
-                    if (mStateHolder.getTip().getVenue() == null) {
-                        mStateHolder.getTip().setVenue(new Venue());
-                    }
-                    mStateHolder.getTip().getVenue().setName(
-                        getIntent().getExtras().getString(EXTRA_VENUE_NAME));
-                }
-                
             } else {
                 Log.e(TAG, "TipActivity requires a tip pareclable in its intent extras.");
                 finish();
@@ -137,13 +102,8 @@ public class TipActivity extends Activity {
     public void onPause() {
         super.onPause();
         
-        if (mResourcesObserver != null) {
-            mRrm.deleteObserver(mResourcesObserver);
-        }
-        
         if (isFinishing()) {
             unregisterReceiver(mLoggedOutReceiver);
-            mHandler.removeCallbacks(mRunnableUpdateUserPhoto);
             stopProgressBar();
         }
     }
@@ -159,77 +119,49 @@ public class TipActivity extends Activity {
         llHeader.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                showUserDetailsActivity(mStateHolder.getTip().getUser().getId());
+                showVenueDetailsActivity(mStateHolder.getTip().getVenue());
             }
         });
         
         TextView tvTitle = (TextView)findViewById(R.id.tipActivityName);
-        tvTitle.setText(
-            getResources().getString(R.string.tip_activity_by) + " " +
-            StringFormatters.getUserFullName(mStateHolder.getTip().getUser()));
+        //tvTitle.setText(
+        //    getResources().getString(R.string.tip_activity_by) + " " +
+        //    StringFormatters.getUserFullName(mStateHolder.getTip().getUser()));
+        tvTitle.setText(mStateHolder.getTip().getVenue().getName());
         
-        setUserPhoto(true);
+        TextView tvAddress = (TextView)findViewById(R.id.tipActivityAddress);
+        tvAddress.setText(mStateHolder.getTip().getVenue().getAddress());
         
-        mListAdapter = new TipActivityAdapter(
-            this, 
-            mStateHolder.getTip().getVenue() != null ? 
-                mStateHolder.getTip().getVenue().getName() : "",
-            mStateHolder.getTip().getText());
+        TextView tvBody = (TextView)findViewById(R.id.tipActivityBody);
+        tvBody.setText(mStateHolder.getTip().getText());
         
-        mListView = (ListView)findViewById(R.id.tipActivityListView);
-        mListView.setAdapter(mListAdapter);
-        mListView.setOnItemClickListener(new OnItemClickListener() {
+        TextView tvDate = (TextView)findViewById(R.id.tipActivityDate);
+        tvDate.setText(mStateHolder.getTip().getCreated());
+        
+        Button btnAddTodoList = (Button)findViewById(R.id.tipActivityyAddTodoList);
+        btnAddTodoList.setOnClickListener(new OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-                switch (position) {
-                    case TipActivityAdapter.ACTION_ID_ADD_TODO_LIST:
-                        mStateHolder.startTipTask(TipActivity.this, mStateHolder.getTip().getId(), 
-                                TipActivityAdapter.ACTION_ID_ADD_TODO_LIST);
-                        break;
-                    case TipActivityAdapter.ACTION_ID_IVE_DONE_THIS:
-                        mStateHolder.startTipTask(TipActivity.this, mStateHolder.getTip().getId(), 
-                                TipActivityAdapter.ACTION_ID_IVE_DONE_THIS);
-                        break;
-                }
+            public void onClick(View v) {
+                mStateHolder.startTipTask(TipActivity.this, mStateHolder.getTip().getId(), 
+                        TipTask.ACTION_TODO);
+            }
+        });
+        
+        Button btnIveDoneThis = (Button)findViewById(R.id.tipActivityIveDoneThis);
+        btnIveDoneThis.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mStateHolder.startTipTask(TipActivity.this, mStateHolder.getTip().getId(), 
+                        TipTask.ACTION_DONE);
             }
         });
     }
     
-    private void setUserPhoto(boolean fetchIfMissing) {
-        
-        ImageView iv = (ImageView)findViewById(R.id.tipActivityPhoto);
-        
-        User user = mStateHolder.getTip().getUser();
-        if (user != null) {
-            try {
-                Bitmap bitmap = BitmapFactory.decodeStream(mRrm.getInputStream(Uri.parse(user
-                        .getPhoto())));
-                iv.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                setUserPhotoNone(user);
-                
-                // Try to fetch the photo.
-                mResourcesObserver = new RemoteResourceManagerObserver();
-                mRrm.addObserver(mResourcesObserver);
-                mRrm.request(Uri.parse(user.getPhoto()));
-            }
-        }
-    }
-    
-    private void setUserPhotoNone(User user) {
-        ImageView iv = (ImageView)findViewById(R.id.tipActivityPhoto);
-        if (Foursquare.MALE.equals(user.getGender())) {
-            iv.setImageResource(R.drawable.blank_boy);
-        } else {
-            iv.setImageResource(R.drawable.blank_girl);
-        }
-    }
-    
-    private void showUserDetailsActivity(String userId) {
-        Intent intent = new Intent(this, UserDetailsActivity.class);
-        intent.putExtra(UserDetailsActivity.EXTRA_USER_ID, userId);
-        intent.putExtra(UserDetailsActivity.EXTRA_SHOW_ADD_FRIEND_OPTIONS, true);
-        startActivity(intent);
+    private void showVenueDetailsActivity(Venue venue) {
+        //Intent intent = new Intent(this, UserDetailsActivity.class);
+        //intent.putExtra(UserDetailsActivity.EXTRA_USER_ID, userId);
+        //intent.putExtra(UserDetailsActivity.EXTRA_SHOW_ADD_FRIEND_OPTIONS, true);
+        //startActivity(intent);
     }
     
     private void startProgressBar(int task) {
@@ -237,13 +169,13 @@ public class TipActivity extends Activity {
             
             String message = "";
             switch (task) {
-                case TipActivityAdapter.ACTION_ID_ADD_TODO_LIST:
-                    message = getResources().getString(
-                        R.string.tip_activity_action_todo);
+                case TipTask.ACTION_TODO:
+  //                  message = getResources().getString(
+  //                      R.string.tip_activity_action_todo);
                     break;
-                case TipActivityAdapter.ACTION_ID_IVE_DONE_THIS:
-                    message = getResources().getString(
-                        R.string.tip_activity_action_done_this);
+                case TipTask.ACTION_DONE:
+  //                  message = getResources().getString(
+  //                      R.string.tip_activity_action_done_this);
                     break;
             }
 
@@ -266,12 +198,12 @@ public class TipActivity extends Activity {
         if (tip != null) {
             String message = "";
             switch (type) {
-                case TipActivityAdapter.ACTION_ID_ADD_TODO_LIST:
+                case TipTask.ACTION_TODO:
                     message = getResources().getString(
                             R.string.tip_activity_prgoress_complete_todo);
                     setResult(RESULT_TIP_MARKED_TODO);
                     break;
-                case TipActivityAdapter.ACTION_ID_IVE_DONE_THIS:
+                case TipTask.ACTION_DONE:
                     message = getResources().getString(
                             R.string.tip_activity_prgoress_complete_done);
                     setResult(RESULT_TIP_MARKED_DONE);
@@ -288,6 +220,9 @@ public class TipActivity extends Activity {
         private String mTipId;
         private int mTask;
         private Exception mReason;
+        
+        public static final int ACTION_TODO = 0;
+        public static final int ACTION_DONE = 1;
 
         public TipTask(TipActivity activity, String tipid, int task) {
             mActivity = activity;
@@ -316,10 +251,10 @@ public class TipActivity extends Activity {
 
                 Tip tip = null;
                 switch (mTask) {
-                    case TipActivityAdapter.ACTION_ID_ADD_TODO_LIST:
+                    case ACTION_TODO:
                         tip = foursquare.tipMarkTodo(mTipId);
                         break;
-                    case TipActivityAdapter.ACTION_ID_IVE_DONE_THIS:
+                    case ACTION_DONE:
                         tip = foursquare.tipMarkDone(mTipId);
                         break;
                 }
@@ -390,18 +325,4 @@ public class TipActivity extends Activity {
             return mIsRunningTipTask;
         }
     }
-    
-    private class RemoteResourceManagerObserver implements Observer {
-        @Override
-        public void update(Observable observable, Object data) {
-            mHandler.post(mRunnableUpdateUserPhoto);
-        }
-    }
-    
-    private Runnable mRunnableUpdateUserPhoto = new Runnable() {
-        @Override
-        public void run() {
-            setUserPhoto(false);
-        }
-    };
 }
