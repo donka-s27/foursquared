1c018478e52752a7918c4008dc7f7fa63eb7c109
markww <markww@gmail.com>
2010-09-25 04:41:17 -0400

    Fixed parser for user invites, removed fading edges on listviews.

--- main/src/com/joelapenna/foursquare/parsers/json/FriendInvitesResultParser.java
+++ main/src/com/joelapenna/foursquare/parsers/json/FriendInvitesResultParser.java
@@ -3,10 +3,14 @@
  */
 package com.joelapenna.foursquare.parsers.json;
 
+import com.joelapenna.foursquare.types.Emails;
 import com.joelapenna.foursquare.types.FriendInvitesResult;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.util.Log;
 
 
 /**
@@ -18,6 +22,7 @@ public class FriendInvitesResultParser extends AbstractParser<FriendInvitesResul
     
     @Override
     public FriendInvitesResult parse(JSONObject json) throws JSONException {
+        
         FriendInvitesResult obj = new FriendInvitesResult();
         if (json.has("users")) {
             obj.setContactsOnFoursquare(
@@ -25,12 +30,21 @@ public class FriendInvitesResultParser extends AbstractParser<FriendInvitesResul
                     new UserParser()).parse(json.getJSONArray("users")));
         } 
         if (json.has("emails")) {
-            obj.setContactEmailsOnNotOnFoursquare(new EmailsParser().parse(json.getJSONObject("emails")));
-        } 
-        if (json.has("invited")) {
-            obj.setContactEmailsOnNotOnFoursquareAlreadyInvited(new EmailsParser().parse(json.getJSONObject("invited")));
+            Emails emails = new Emails();
+            JSONArray array = json.getJSONArray("emails");
+            for (int i = 0; i < array.length(); i++) {
+                emails.add(emails.get(i));
+            }
+            obj.setContactEmailsOnNotOnFoursquare(emails);
         }
-                
+        if (json.has("invited")) {
+            Emails emails = new Emails();
+            JSONArray array = json.getJSONArray("invited");
+            for (int i = 0; i < array.length(); i++) {
+                emails.add(emails.get(i));
+            }
+            obj.setContactEmailsOnNotOnFoursquareAlreadyInvited(emails);
+        }
         return obj;
     }
 }
